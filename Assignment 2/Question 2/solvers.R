8211466061e53ffe621f5d14b70f662f0c411078
m <m.rooney93@gmail.com>
2017-12-29 06:40:46 -0800

    Gauss Seidel solver works

--- Assignment 2/Question 2/solvers.R
+++ Assignment 2/Question 2/solvers.R
@@ -29,29 +29,48 @@ gen_b_matrix <- function(){
 
 # Algorithm taken from https://en.wikipedia.org/wiki/Jacobi_method#Algorithm
 # TODO: use tolerance
-jacobi_solver <- function(a_mat, b_mat, guess_mat, max_iter, tol, len){
-	guess_mat_t = guess_mat;
-	guess_mat_t1 <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0); # filled in later
+# TODO: create guess using len
+jacobi_solver <- function(a_mat, b_mat, max_iter, tol, len){
+	guess_mat <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0); 
+	guess_mat_next <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0); # filled in later
 	for(iter in 1:max_iter){
 		for(i in 1:len){
 			sigma <- 0;
 			for(j in 1:len){
 				if(i != j){
-					sigma = sigma + (a_mat[i, j] * guess_mat_t[j]);
+					sigma = sigma + (a_mat[i, j] * guess_mat[j]);
 				}
 			}	
-			guess_mat_t1[i] = (1 / a_mat[i, i]) * (b_mat[i] - sigma);
+			guess_mat_next[i] = (1 / a_mat[i, i]) * (b_mat[i] - sigma);
 		}
-		guess_mat_t <- guess_mat_t1;
-		guess_mat_t1 <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0); # filled in later
+		guess_mat <- guess_mat_next;
 	}
-	return(guess_mat_t);
+	return(guess_mat);
+}
+
+# Algorithm taken from https://en.wikipedia.org/wiki/Gauss%E2%80%93Seidel_method#Algorithm
+# TODO: use tolerance
+# TODO: create guess using len
+gauss_seidel_solver <- function(a_mat, b_mat, max_iter, tol, len){
+	result <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	for(iter in 1:max_iter){
+		for(i in 1:len){
+			sigma <- 0;
+			for(j in 1:len){
+				if(i != j){
+					sigma <- sigma + (a[i, j] * result[j]);
+				}
+			}
+			result[i] <- (1 / a[i, i]) * (b[i] - sigma);
+		}
+	}
+	return(result);
 }
 
 do_work <- function(){
 	a_mat <- gen_a_matrix();
 	b_mat <- gen_b_matrix();
-	guess_mat <- c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-	result <- jacobi_solver(a_mat, b_mat, guess_mat, 100, 0.01, 16);
+	#result <- jacobi_solver(a_mat, b_mat, 100, 0.01, 16);
+	result <- gauss_seidel_solver(a_mat, b_mat, 100, 0.01, 16);
 	return(result);
 }
